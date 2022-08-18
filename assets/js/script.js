ea18d374d1fee356b7aef3b175841080048a9ca0
Gabe Vee <0gabevee0@gmail.com>
2022-08-17 21:23:38 -0600

    added forcast call and convert city name into lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,86 @@
-//Variable Declerations
-var API_KEY = "704f9597167e48ceb527d58b08526b5f";
-
+//Variable Declarations
+var API_KEY = "1d41d88a7537cedbb3f07e92b34b8e94";
+var APIKEY = "704f9597167e48ceb527d58b08526b5f";
+var weatherItem = $("p");
+var cityName = $("#currentDay");
+// lat=39.7294&lon=-104.8125
 //Functions
-function getWeatherApi() {
-  var url =
-    "https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely";
+function getWeatherApi(location) {
+  var searchResult = location; //location
+  var openWeatherUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    searchResult +
+    "&appid=" +
+    API_KEY;
 
-  fetch(url)
+  fetch(openWeatherUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
+      console.log(data.name);
+      var lon = data.coord.lon;
+      var lat = data.coord.lat;
+
+      var weatherBit =
+        "https://api.weatherbit.io/v2.0/current?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&key=" +
+        APIKEY;
+
+      fetch(weatherBit)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data.data[0]);
+          console.log(data.data[0].weather.icon);
+          console.log(data.data[0].datetime);
+          console.log(data.data[0].temp);
+          console.log(data.data[0].wind_spd);
+          console.log(data.data[0].rh);
+          console.log(data.data[0].uv);
+          cityName.text(
+            data.data[0].city_name +
+              " " +
+              data.data[0].datetime +
+              " " +
+              data.data[0].weather.icon
+          );
+          var weatherArray = [
+            `Temp: ${data.data[0].temp} F`,
+            `Wind: ${data.data[0].wind_spd} MPH`,
+            `Humidity: ${data.data[0].rh} %`,
+            `UV: ${data.data[0].uv}`,
+          ];
+          for (var i = 0; i < weatherArray.length; i++) {
+            weatherItem.eq(i).text(weatherArray[i]);
+          }
+        });
+
+      var forcast =
+        "https://api.weatherbit.io/v2.0/forecast/daily?&lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&key=" +
+        APIKEY;
+      fetch(forcast)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data.data);
+          console.log(data.data[1]);
+          console.log(data.data[2]);
+          console.log(data.data[3]);
+          console.log(data.data[4]);
+          console.log(data.data[5]);
+        });
     });
 }
-getWeatherApi();
-
-function getUVIndex() {
-  var lat = $("#lat").val();
-  var lng = $("#lng").val();
-  var alt = $("#alt").val();
-  var ozone = $("#ozone").val();
-  var dt = $("#dt").val();
-
-  $.ajax({
-    type: "GET",
-    dataType: "json",
-    beforeSend: function (request) {
-      request.setRequestHeader(
-        "x-access-token",
-        "bed77c07b72ceed9e0f50531d3cf941a"
-      );
-    },
-    url:
-      "https://api.openuv.io/api/v1/uv?lat=" +
-      lat +
-      "&lng=" +
-      lng +
-      "&alt=" +
-      alt +
-      "&ozone=" +
-      ozone +
-      "&dt=" +
-      dt,
-    success: function (response) {
-      //handle successful response
-    },
-    error: function (response) {
-      // handle error response
-    },
-  });
-}
+getWeatherApi("aurora");
 //Event Listeners
